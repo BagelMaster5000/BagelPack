0c1646851b8a2a076a1bb0b9f4de6e9a69c6a526
Sammy Mahmoudi <bagelmaster5001@gmail.com>
2020-11-02 18:40:18 -0600

    Initial Commit

---
+++ Runtime/Scripts/ParticlesDetachAndDestroy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+// When activated, detaches from all parents, plays particle effect from attached ParticleSystem, and destroys itself when finished.
+[RequireComponent(typeof(ParticleSystem))]
+public class ParticlesDetachAndDestroy : MonoBehaviour
+{
+    ParticleSystem particles;
+
+    public void Activate()
+    {
+        transform.localScale = Vector3.one;
+        this.transform.parent = null;
+        particles = GetComponent<ParticleSystem>();
+        particles.Play();
+        StartCoroutine(DestroyWhenDone());
+    }
+
+    IEnumerator DestroyWhenDone()
+    {
+        while (particles.isPlaying)
+            yield return null;
+        Destroy(this.gameObject);
+    }
+}
---
+++ Runtime/Scripts/SoundDetachAndDestroy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// When activated, detaches from all parents, plays sound from attached AudioSource, and destroys itself when finished.
+[RequireComponent(typeof(AudioSource))]
+public class SoundDetachAndDestroy : MonoBehaviour
+{
+    AudioSource sound;
+
+    public void Activate()
+    {
+        this.transform.parent = null;
+        sound = GetComponent<AudioSource>();
+        sound.Play();
+        StartCoroutine(DestroyWhenSilent());
+    }
+
+    IEnumerator DestroyWhenSilent()
+    {
+        while (sound.isPlaying)
+            yield return null;
+        Destroy(this.gameObject);
+    }
+}
